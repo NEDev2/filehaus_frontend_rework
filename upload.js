24fb3215ad722ee10862546ffc7b16a4334c78ed
Nehoray_Developer <66194934+NEDev2@users.noreply.github.com>
2024-06-16 02:13:37 +0300

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,69 @@
+(function () {
+  "use strict";
+
+  let fileInput, uploadProgress, message;
+
+  function init() {
+    fileInput = document.getElementById("filetoupload");
+    uploadProgress = document.getElementById("upload-progress");
+    message = document.getElementById("message");
+
+    fileInput.addEventListener("change", handleFileUpload);
+  }
+
+  function handleFileUpload() {
+    const file = fileInput.files[0];
+    const xhr = new XMLHttpRequest();
+    let startTime = performance.now();
+
+    xhr.upload.onloadstart = function (e) {
+      showUploadProgress(e);
+      startTime = performance.now();
+    };
+
+    xhr.upload.onprogress = function (e) {
+      updateUploadProgress(e, startTime);
+    };
+
+    xhr.upload.onloadend = function () {
+      hideUploadProgress();
+      message.textContent = "Processing";
+      fileInput.disabled = false;
+    };
+
+    xhr.onload = function () {
+      message.textContent = xhr.responseText;
+    };
+
+    xhr.open("PUT", `api/upload/${file.name}`, true);
+    xhr.setRequestHeader("Content-Type", file.type);
+    xhr.send(file);
+  }
+
+  function showUploadProgress(e) {
+    uploadProgress.classList.add("visible");
+    uploadProgress.value = 0;
+    uploadProgress.max = e.total;
+    message.textContent = "Uploading...";
+    fileInput.disabled = true;
+  }
+
+  function updateUploadProgress(e, startTime) {
+    uploadProgress.value = e.loaded;
+    uploadProgress.max = e.total;
+
+    const elapsedSeconds = (performance.now() - startTime) / 1000;
+    const uploadSpeed = e.loaded / (1024 * 1024 * elapsedSeconds);
+    const percentage = (e.loaded / e.total) * 100;
+
+    message.textContent = `Upload Speed: ${uploadSpeed.toFixed(
+      2
+    )} MB/s || ${percentage.toFixed(2)}% done`;
+  }
+
+  function hideUploadProgress() {
+    uploadProgress.classList.remove("visible");
+  }
+
+  init();
+})();
